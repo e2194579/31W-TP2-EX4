f0f3ad0d290eaa0f99583245e3ed700dae927e18
RBC <renaud_b-c@hotmail.com>
2022-08-15 17:37:57 -0400

    Ajout du fichier index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?> 
+<h1>index.php</h1>
+<?php
+if (have_posts()):
+    while(have_posts()) : the_post(); ?>
+        <h2><?php the_title(); ?></h2>
+    <?php endwhile; ?>
+<?php endif; ?>    
+<?php get_footer(); ?>
